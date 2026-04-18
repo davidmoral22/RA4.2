28214a9d226f0bdab1d4fe89216f767afebd2215
jesuscanterodam10 <jesuscanterodam@gmail.com>
2026-04-18 16:45:29 +0200

    Segundo Error Solucionado CheckStyle

--- src/ar/charlycimino/ejemplos/excepcionespropias/Calculadora.java
+++ src/ar/charlycimino/ejemplos/excepcionespropias/Calculadora.java
@@ -8,17 +8,24 @@ package ar.charlycimino.ejemplos.excepcionespropias;
  * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
  */
 public class Calculadora {
-    public int factorial (final int num) {
+    public int factorial(final int num) {
         if (num < 0) {
-            throw new ArithmeticException("No se puede calcular el factorial de un número negativo");
+            // Dividimos el texto en dos líneas
+            throw new ArithmeticException("No se puede calcular el "
+                    + "factorial de un número negativo");
         }
+
         if (num > 12) {
-            throw new IntegerOverflowException("Por limitaciones técnicas, no se puede calcular el factorial de un entero mayor que 12");
+            // Dividimos el texto para no superar los 80 caracteres
+            throw new IntegerOverflowException("Por limitaciones técnicas, no se "
+                    + "puede calcular el factorial de un entero mayor que 12");
         }
+
         int fact = 1;
         for (int i = 2; i <= num; i++) {
             fact = fact * i;
         }
+
         return fact;
     }
 }
